9bc8c5da7afd5b4028c98746564740b55803a9fe
Moritz Moeller <virtualritz@protonmail.com>
2024-06-13 16:26:00 +0200

    Fixed test.

--- rust/src/api/mod.rs
+++ rust/src/api/mod.rs
@@ -4,17 +4,17 @@ pub mod metadata;
 
 #[cfg(test)]
 mod tests {
-    use crate::api::FileInfo;
+    use crate::api::file_info::FileInfo;
+    use std::path::PathBuf;
     use anyhow::Result;
-    use std::path::Path;
 
     #[test]
     fn it_works() -> Result<()> {
         let jpg =
-            File::new(Path::new("assets/gamma_dalai_lama_gray_good.jpg"))?;
+            FileInfo::try_from(PathBuf::from("assets/gamma_dalai_lama_gray_good.jpg"))?;
         println!("JPG\n{:?}", jpg.metadata());
 
-        let exr = File::new(Path::new("assets/j0.3toD.exr"))?;
+        let exr = FileInfo::try_from(PathBuf::from("assets/j0.3toD.exr"))?;
         println!("EXR\n{:?}", exr.metadata());
 
         Ok(())
